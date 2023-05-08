58b40e252a6fc00deba53a8817420f61b6799579
kilgette <kilgette@gmail.com>
2023-05-07 21:06:39 -0500

    post tutor updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button = $("#submit-button");
 const APIkey = "279df7938b24538a3425a8db10b868c0"
-let cityName = "Minneapolis";
+let cityName = $("#search-bar");
+let todayWeather=$(".today");
 cityHistory = JSON.parse(localStorage.getItem("cities"));
 if (cityHistory === null) {
   cityHistory = [];
@@ -9,7 +10,7 @@ if (cityHistory === null) {
 // button.on('click', searchCity())
 button.on("click", function (event) {
   event.preventDefault();
-  searchCity();
+  retrieveWeather(cityName.val());
 });
 
 
@@ -19,43 +20,80 @@ function historySearch(event) {
   weather();
 }
 
-//Geocoding API
-fetch(
-  'https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={279df7938b24538a3425a8db10b868c0}&units=imperial'
-    .then((response) => response.json())
-    .then(function (data) {
-      startWeather();
 
 //Grab weather data for next 5 days
-function retrieveWeather() {          
+function retrieveWeather(cityName) {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=279df7938b24538a3425a8db10b868c0&units=imperial')
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+
+      startWeather();
+    })
+  fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=279df7938b24538a3425a8db10b868c0&units=imperial')
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data){
+      console.log(data);
+      todayWeather.html(` <h2>${data.name} (${dayjs.unix(data.dt).format("MM/DD/YYYY")}) <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></h2>
+      <p>Temp: ${data.main.temp}degrees F</p>
+      <p>Humidity: 30%</p>
+      <p>Wind: 30 mpg</p>`)
+    })
 }
 
 //Display current weather data on the page
 function displayWeather(weatherData) {
   let city = data.city.name;
   console.log(weatherData);
-  }
-
-//Get lat and long data     
-fetch(
-  api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid={APIkey}&units=imperial
-    )
-
-  // info needed from call
-  // list.main.temp
-  // list.main.temp_min
-  // list.main.temp_max
-  // list.main.humidity
-  // "main":{
-  //   "temp":306.15, //current temperature
-  //   "humidity":44,
-  //   "temp_min":306, //min current temperature in the city
-  //   "temp_max":306 //max current temperature in the city
-  // },
-  // list.wind.speed
-  // list.weather
-  // city.name
-
 }
 
+//Get lat and long data     
 
+// info needed from call
+// list.main.temp
+// list.main.temp_min
+// list.main.temp_max
+// list.main.humidity
+// "main":{
+//   "temp":306.15, //current temperature
+//   "humidity":44,
+//   "temp_min":306, //min current temperature in the city
+//   "temp_max":306 //max current temperature in the city
+// },
+// list.wind.speed
+// list.weather
+// city.name
+
+
+
+
+// This is the array of hour blocks: 8 per day, for a total of 40.
+const daysInForecast = sample.list
+
+/*
+Each date object has a property called "dt", which is a Unix timestamp for the date and time 
+of that object's data. The first one is 1681333200.
+*/
+
+// Create a new array to hold one day block per forecast day.
+const newForecastArr = []
+
+// iterate over the 40 blocks, but we will do them 8 at a time, so that we get one per day.
+for (let i = 0; i < 40; i = i + 8) {
+  newForecastArr.push(sample.list[i])
+}
+
+// We now have a new array with one record for each day!
+console.log(newForecastArr)
+
+
+/* 
+Want to see why arrow functions are cool? Combined with an array method you haven't learned 
+yet, we can do all this work in one line of code. We will show you array.filter() later!
+*/
+
+const newForecastArr2 = sample.list.filter((_dayObj, idx) => idx % 8 === 0)
+console.log(newForecastArr2)
